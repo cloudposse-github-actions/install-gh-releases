8a78c0c6ec7925bc4651ea32af8725c65563ffef
Igor Rodionov <goruha@gmail.com>
2024-08-14 08:26:00 +0200

    Fix tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,6 +9,8 @@
 import * as core from '@actions/core'
 import * as main from '../src/main'
 import * as octokit from "@octokit/core";
+import * as os from "os";
+import * as path from "path";
 import * as github from "../src/github";
 import * as tc from "@actions/tool-cache";
 import fs from "fs";
@@ -41,6 +43,8 @@ describe('action', () => {
         setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
         octokitMock = jest.spyOn(github, 'getOctokit').mockImplementation()
         cacheMock = jest.spyOn(tc, 'downloadTool').mockImplementation()
+
+        process.env['RUNNER_TOOL_CACHE'] = os.tmpdir()
     })
 
     it('sets the time output', async () => {
@@ -68,18 +72,18 @@ wasmerio/wasmer: {}
             resolve({status: 302, headers: {location: 'mock-url'}});
         })
 
-        let getReleaseCallback = jest.fn().mockImplementation((owner, repo) => {
-            switch (repo) {
+        let getReleaseCallback = jest.fn().mockImplementation((inputs) => {
+            switch (inputs.repo) {
                 case 'connecti':
-                    return {data: JSON.parse(fs.readFileSync("fixtures/connecti.latest.json", 'utf8'))}
+                    return {data: JSON.parse(fs.readFileSync("__tests__/fixtures/connecti.latest.json", 'utf8'))}
                 case 'wasmer':
-                    return {data: JSON.parse(fs.readFileSync("fixtures/wasmer.latest.json", 'utf8'))}
+                    return {data: JSON.parse(fs.readFileSync("__tests__/fixtures/wasmer.latest.json", 'utf8'))}
                 case 'change-aws-credentials':
-                    return {data: JSON.parse(fs.readFileSync("fixtures/change-aws-credentials.v1.18.0.json", 'utf8'))}
+                    return {data: JSON.parse(fs.readFileSync("__tests__/fixtures/change-aws-credentials.v1.18.0.json", 'utf8'))}
                 case 'tfsec':
-                    return {data: JSON.parse(fs.readFileSync("fixtures/tfsec.v1.18.0.json", 'utf8'))}
-                default:
                     return {data: JSON.parse(fs.readFileSync("__tests__/fixtures/tfsec.v1.18.0.json", 'utf8'))}
+                default:
+                    null
             }
         })
 
@@ -94,10 +98,38 @@ wasmerio/wasmer: {}
             } as unknown as octokit.Octokit
         })
 
-        cacheMock.mockImplementation( (url: string, dest?: string, auth?: string, headers?: OutgoingHttpHeaders) => {
-            return new Promise<string>((resolve, reject) => {
-                resolve('mock-path')
-            })
+        cacheMock.mockImplementation( async (url: string, dest?: string, auth?: string, headers?: OutgoingHttpHeaders) => {
+            const tmpdir = os.tmpdir();
+            let filepath = ""
+            switch (url) {
+                case 'https://api.github.com/repos/aquasecurity/tfsec/releases/assets/61869003':
+                    filepath = path.join(tmpdir, 'tfsec-checkgen-linux-amd64');
+                    fs.copyFile("__tests__/fixtures/assets/tfsec-checkgen-linux-amd64", filepath,
+                        (err) => { if (err) throw err; }
+                    );
+                    return filepath
+                case 'https://api.github.com/repos/jaxxstorm/connecti/releases/assets/149202168':
+                    filepath = path.join(tmpdir, 'connecti-v0.0.4-darwin-amd64.tar.gz');
+                    fs.copyFile("__tests__/fixtures/assets/connecti-v0.0.4-darwin-amd64.tar.gz", filepath,
+                        (err) => { if (err) throw err; }
+                    );
+                    return filepath
+                case 'https://api.github.com/repos/jaxxstorm/change-aws-credentials/releases/assets/99210524':
+                    filepath = path.join(tmpdir, 'change-aws-credentials-v0.4.0-darwin-amd64.tar.gz');
+                    fs.copyFile("__tests__/fixtures/assets/change-aws-credentials-v0.4.0-darwin-amd64.tar.gz", filepath,
+                        (err) => { if (err) throw err; }
+                    );
+                    return filepath
+                case 'https://api.github.com/repos/wasmerio/wasmer/releases/assets/179961899':
+                    filepath = path.join(tmpdir, 'wasmer-darwin-amd64.tar.gz');
+                    fs.copyFile("__tests__/fixtures/assets/wasmer-darwin-amd64.tar.gz", filepath,
+                        (err) => { if (err) throw err; }
+                    );
+                    return filepath
+
+                default:
+                    return ''
+            }
         })
 
         await main.run()
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import * as core from "@actions/core";
 import Ajv2020 from "ajv/dist/2020"
 import * as yaml from 'js-yaml';
 import * as tc from "@actions/tool-cache";
-import {debug} from "@actions/core";
 
 interface ToolInfo {
     owner: string;
@@ -23,6 +22,7 @@ interface Config {
     tag: string
     platform: string
     arch: string
+    arch_list: string[]
     extension: string
     extension_matching: boolean
     rename_to: string
@@ -57,53 +57,54 @@ export async function run() {
         const cacheEnabled = (core.getInput("cache") === "enable")
 
         let configs : Map<string, Config> = new Map<string, Config>();
-
         for (let repo in (configJson as object))  {
             let config = (configJson as object)[repo];
-            // tag = !tag ? "latest" : tag
-            if (config == null) {
-                configs[repo] = {
+            if (config === null) {
+                configs.set(repo, {
                     owner: repo.split('/')[0],
                     project: repo.split('/')[1],
                     tag: "latest",
                     platform: defaultPlatform(),
                     arch: defaultArch(),
+                    arch_list: defaultArchList(),
                     extension: "",
                     extension_matching: false,
                     rename_to: "",
                     chmod: "",
-                }
-            } else if (config.type === 'string') {
-                configs[repo] = {
+                } as Config)
+            } else if (typeof config === 'string') {
+                configs.set(repo, {
                     owner: repo.split('/')[0],
                     project: repo.split('/')[1],
                     tag: config === "" ? "latest" : config,
                     platform: defaultPlatform(),
                     arch: defaultArch(),
+                    arch_list: defaultArchList(),
                     extension: "",
                     extension_matching: false,
                     rename_to: "",
                     chmod: "",
-                }
-            } else if (config.type == 'object') {
-                configs[repo] = {
+                } as Config)
+            } else if (typeof config == 'object') {
+                configs.set(repo, {
                     owner: repo.split('/')[0],
                     project: repo.split('/')[1],
-                    tag: config.tag === "" ? "latest" : config.tag,
-                    platform: config.platform === "" ? defaultPlatform() : config.platform,
-                    arch: config.arch === "" ? defaultArch() : config.arch,
-                    extension: config.extension,
+                    tag: config.tag === "" || config.tag === undefined ? "latest" : config.tag,
+                    platform: config.platform === "" || config.platform === undefined ? defaultPlatform() : config.platform,
+                    arch: config.arch === "" || config.arch === undefined ? defaultArch() : config.arch,
+                    arch_list: config.arch === "" || config.arch === undefined ? defaultArchList() : [config.arch],
+                    extension: config.extension === undefined ? "" : config.extension,
                     extension_matching: config.extension_matching === "enable" || config.extension_matching === "true",
-                    rename_to: config.rename_to,
-                    chmod: config.chmod,
-                }
+                    rename_to: config.rename_to === undefined ? "" : config.rename_to,
+                    chmod: config.chmod === undefined ? "" : config.chmod,
+                } as Config)
             }
         }
-        const octokit = getOctokit(token)
-        for (let repo in configs)  {
-            downloadRelease(octokit, configs[repo], cacheEnabled, binariesLocation)
-        }
 
+        const octokit = getOctokit(token)
+        for (let [repo, config] of configs) {
+            await  downloadRelease(octokit, config, cacheEnabled, binariesLocation)
+        }
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
@@ -128,6 +129,10 @@ function defaultPlatform() {
 }
 
 function defaultArch() {
+    return os.arch()
+}
+
+function defaultArchList() {
     switch (os.arch()) {
         case "x64":
             return ["x86_64", "x64", "amd64"]
@@ -135,7 +140,6 @@ function defaultArch() {
             return [os.arch()]
     }
 }
-
 async function downloadRelease(octokit, config: Config, cache_enabled: boolean, binary_location: string): Promise<boolean> {
 
     let dest = toolPath(config);
@@ -145,14 +149,14 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
         core.debug(`==> Given bin location: ${binary_location}`);
         finalBinLocation = path.join(dest, binary_location);
     }
-    core.debug(`==> Binaries will be located at: ${finalBinLocation}`);
+    core.info(`==> Binaries will be located at: ${finalBinLocation}`);
 
     let cacheKey = cachePrimaryKey(config);
     if (cache_enabled && cacheKey !== undefined) {
         let ok = await cache.restoreCache([dest], cacheKey);
         if (ok !== undefined) {
-            core.debug(`Found ${config.project} in the cache: ${dest}`)
-            core.debug(`Adding ${finalBinLocation} to the path`);
+            core.info(`Found ${config.project} in the cache: ${dest}`)
+            core.info(`Adding ${finalBinLocation} to the path`);
             core.addPath(finalBinLocation);
             return false;
         }
@@ -177,20 +181,20 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
     if (config.extension_matching) {
         if (config.extension === "") {
             extMatchRegexForm = "\.(tar.gz|zip|tgz)";
-            core.debug(`==> Using default file extension matching: ${extMatchRegexForm}`);
+            core.info(`==> Using default file extension matching: ${extMatchRegexForm}`);
         } else {
             extMatchRegexForm = config.extension;
-            core.debug(`==> Using custom file extension matching: ${extMatchRegexForm}`);
+            core.info(`==> Using custom file extension matching: ${extMatchRegexForm}`);
         }
     } else {
-        core.debug("==> File extension matching disabled");
+        core.info("==> File extension matching disabled");
     }
 
-    let osMatch = [config.platform].concat(config.arch)
+    let osMatch = [config.platform].concat(config.arch_list)
     let osMatchRegexForm = `(${osMatch.join('|')})`
     let re = new RegExp(`${osMatchRegexForm}.*${osMatchRegexForm}.*${extMatchRegexForm}`)
     let asset = getReleaseUrl.data.assets.find(obj => {
-        core.debug(`searching for ${obj.name} with ${re.source}`)
+        core.info(`searching for ${obj.name} with ${re.source}`)
         let normalized_obj_name = obj.name.toLowerCase()
         return re.test(normalized_obj_name)
     })
@@ -204,7 +208,7 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
 
     const url = asset.url
 
-    core.debug(`Downloading ${config.project} from ${url}`)
+    core.info(`Downloading ${config.project} from ${url}`)
     const binPath = await tc.downloadTool(url,
         undefined,
         `token ${octokit.token}`,
@@ -218,12 +222,12 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
         // Release is an archive file so extract it to the destination
         const extractFlags = getExtractFlags(asset.name)
         if (extractFlags !== undefined) {
-            core.debug(`Attempting to extract archive with custom flags ${extractFlags}`)
+            core.info(`Attempting to extract archive with custom flags ${extractFlags}`)
             await extractFn(binPath, dest, extractFlags);
         } else {
             await extractFn(binPath, dest);
         }
-        core.debug(`Automatically extracted release asset ${asset.name} to ${dest}`);
+        core.info(`Automatically extracted release asset ${asset.name} to ${dest}`);
 
         const bins = fs.readdirSync(finalBinLocation, { withFileTypes: true })
             .filter(item => item.isFile())
@@ -240,7 +244,7 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
                 const binPath = path.join(finalBinLocation, bin);
                 try {
                     fs.chmodSync(binPath, config.chmod);
-                    core.debug(`chmod'd ${binPath} to ${config.chmod}`)
+                    core.info(`chmod'd ${binPath} to ${config.chmod}`)
                 } catch (chmodErr) {
                     core.setFailed(`Failed to chmod ${binPath} to ${config.chmod}: ${chmodErr}`);
                 }
@@ -252,13 +256,13 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
         // parameter then this is where we apply those.
         // Regardless of any rename-to parameter we still need to move the download to the actual destination
         // otherwise it won't end up on the path as expected
-        core.debug(
+        core.info(
             `Release asset ${asset.name} did not have a recognised file extension, unable to automatically extract it`)
         try {
             fs.mkdirSync(dest, { 'recursive': true });
 
             const outputPath = path.join(dest, config.rename_to !== "" ? config.rename_to : path.basename(binPath));
-            core.debug(`Created output directory ${dest}`);
+            core.info(`Created output directory ${dest}`);
 
             var moveFailed = false;
 
@@ -281,13 +285,13 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
             }
 
             if (!moveFailed) {
-                core.debug(`Moved release asset ${asset.name} to ${outputPath}`);
+                core.info(`Moved release asset ${asset.name} to ${outputPath}`);
             }
 
             if ((config.chmod !== "") && !moveFailed) {
                 try {
                     fs.chmodSync(outputPath, config.chmod);
-                    core.debug(`chmod'd ${outputPath} to ${config.chmod}`)
+                    core.info(`chmod'd ${outputPath} to ${config.chmod}`)
                 } catch (chmodErr) {
                     core.setFailed(`Failed to chmod ${outputPath} to ${config.chmod}: ${chmodErr}`);
                 }
@@ -305,17 +309,17 @@ async function downloadRelease(octokit, config: Config, cache_enabled: boolean, 
             if (typedError.name === cache.ValidationError.name) {
                 throw error;
             } else if (typedError.name === cache.ReserveCacheError.name) {
-                core.debug(typedError.message);
+                core.info(typedError.message);
             } else {
                 core.warning(typedError.message);
             }
         }
     }
 
-    core.debug(`Adding ${finalBinLocation} to the path`);
+    core.info(`Adding ${finalBinLocation} to the path`);
     core.addPath(finalBinLocation);
-    core.debug(`Successfully installed ${config.project}`);
-    core.debug(`Binaries available at ${finalBinLocation}`);
+    core.info(`Successfully installed ${config.project}`);
+    core.info(`Binaries available at ${finalBinLocation}`);
 
     return true
 }
